2c400d158afb404e730ddffd55069c086af3ddcd
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2024-11-06 20:34:51 +0700

    update network config

--- modules/lab/net.tf
+++ modules/lab/net.tf
@@ -13,23 +13,55 @@ resource "google_compute_subnetwork" "main" {
  project       = local.project_id
 }
 
-resource "google_compute_firewall" "allow-ssh-to-bastion" {
-  name    = "${local.prefix}-allow-ssh-to-bastion"
+resource "google_compute_firewall" "allow-iap-ssh-tunnel-to-jumpbox" {
+  name    = "${local.prefix}-allow-iap-ssh-tunnel-to-jumpbox"
   network = google_compute_network.main.name
 
   allow {
     protocol = "tcp"
     ports    = ["22"]
   }
-
-  source_ranges = local.known-cidrs
-  target_tags   = ["bastion-host"]
+  source_ranges = ["35.235.240.0/20"]
+  target_tags   = ["iapssh"]
 }
+resource "google_compute_firewall" "allow-jumpbox-ingress" {
+  name    = "${local.prefix}-allow-jumpbox-ingress"
+  network = google_compute_network.main.name
+
+  allow {
+    protocol = "icmp"
+  }
+  allow {
+    protocol = "tcp"
+  }
+  allow {
+    protocol = "udp"
+  }
+  source_ranges = local.known-cidrs
+  target_tags   = ["jumpbox"]
+}
+
+resource "google_compute_firewall" "allow-internal" {
+  name    = "${local.prefix}-allow-internal"
+  network = google_compute_network.main.name
+
+  allow {
+    protocol = "icmp"
+  }
+  allow {
+    protocol = "tcp"
+  }
+  allow {
+    protocol = "udp"
+  }
+  source_ranges = [local.network-cidr]
+}
+
 
 # DNS Records
 
 resource "google_dns_record_set" "jumpbox_private" {
-  name    = "jumpbox.${local.prefix}.${local.private-domain}"
+  name    = "jumpbox-${local.prefix}.${local.private-domain}"
   type    = "A"
   ttl     = 300
   managed_zone = local.private-dns-zone
@@ -45,7 +77,7 @@ resource "google_dns_record_set" "jumpbox_public" {
 }
 
 resource "google_dns_record_set" "yba_private" {
-  name    = "yba.${local.prefix}.${local.private-domain}"
+  name    = "yba-${local.prefix}.${local.private-domain}"
   type    = "A"
   ttl     = 300
   managed_zone = local.private-dns-zone
@@ -54,7 +86,7 @@ resource "google_dns_record_set" "yba_private" {
 
 resource "google_dns_record_set" "db_private" {
   count        = local.db-node-count
-  name         = "db${count.index + 1}.${local.prefix}.${local.private-domain}"
+  name         = "db${count.index + 1}-${local.prefix}.${local.private-domain}"
   type         = "A"
   ttl          = 300
   managed_zone = local.private-dns-zone
